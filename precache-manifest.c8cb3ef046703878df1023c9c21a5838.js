01846133bfb5578ebd285791fc946329214f26d9
DmytroTrutenko <55551711+DmytroTrutenko@users.noreply.github.com>
2020-02-26 00:03:52 +0200

    Updates

---
+++ precache-manifest.c8cb3ef046703878df1023c9c21a5838.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c759dfc3714c3bd0fc47039f8e97fee0",
+    "url": "/react_weather/index.html"
+  },
+  {
+    "revision": "7c750a375eb10b860e43",
+    "url": "/react_weather/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "8181af0af34c13a3302c",
+    "url": "/react_weather/static/css/main.74610895.chunk.css"
+  },
+  {
+    "revision": "7c750a375eb10b860e43",
+    "url": "/react_weather/static/js/2.1b42942e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react_weather/static/js/2.1b42942e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8181af0af34c13a3302c",
+    "url": "/react_weather/static/js/main.4361676c.chunk.js"
+  },
+  {
+    "revision": "d343efda49552d716bfa",
+    "url": "/react_weather/static/js/runtime-main.9c522372.js"
+  },
+  {
+    "revision": "6ea4cc0743a7ad2b2b24d17e06f90dc9",
+    "url": "/react_weather/static/media/error.6ea4cc07.png"
+  },
+  {
+    "revision": "40127a5e21b8353d8d01232ef17bd221",
+    "url": "/react_weather/static/media/fon.40127a5e.png"
+  },
+  {
+    "revision": "6c59bf1e04419d264284e76faa76dae9",
+    "url": "/react_weather/static/media/weather.6c59bf1e.png"
+  }
+]);
